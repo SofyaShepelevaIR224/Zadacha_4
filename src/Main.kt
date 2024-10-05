b8ada6e2531567f00874c3ed9a4110e63da8f2b7
SofyaShepelevaIR224 <shepeleva.sofya91@gmail.com>
2024-10-05 18:09:43 +0300

    z4

--- src/Main.kt
+++ src/Main.kt
@@ -1,16 +1,47 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    // Читаем ввод: два числа и оператор
+    println("Введите число1 операция число2 :")
+    val input = readLine() ?: return
+    val parts = input.split(' ')
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-
-        //println-оператор, позволяющий выводить строку
+    // Проверка ввода
+    if (parts.size != 3) {
+        println("Ты ошибся. Используйте формат: число1 операция число2")
+        return
     }
+
+    val num1 = parts[0].toDoubleOrNull()
+    val operation = parts[1][0]
+    val num2 = parts[2].toDoubleOrNull()
+
+    // Проверка корректности чисел
+    if (num1 == null || num2 == null) {
+        println("Ошибка: одно из чисел введено неверно.")
+        return
+    }
+
+    val result: Double? // Переменная для хранения результата
+
+    // Обработка операции с помощью when
+    result = when (operation) {
+        '+' -> num1 + num2 // Сложение
+        '-' -> num1 - num2 // Вычитание
+        '*' -> num1 * num2 // Умножение
+        '/' -> {
+            // Проверка деления на ноль
+            if (num2 != 0.0) {
+                num1 / num2 // Деление
+            } else {
+                println("Ошибка: деление на ноль!")
+                return
+            }
+        }
+        else -> {
+            println("Ошибка: неверная операция")
+            return
+        }
+    }
+
+    // Вывод результата
+    println(result)
 }
